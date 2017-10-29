a367311e9530d8dd36ecc7d1cb1380ff25a140d5
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-10-29 18:25:43 +0530

    black. Squad Website is LIVE!

---
+++ config.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * OVERLAY CONFIG
+ */
+
+/**
+ * OVERLAY SKIN OPTION  [ 'black' : 'white' ]
+ * 'black' = BLACK COLOR SCHEME
+ * 'white' = WHITE COLOR SCHEME
+ */
+var option_overlay_skin = 'black';
+
+
+/**
+ * OVERLAY ANIMATION  [ 'fade' : 'slide' ]
+ * 'fade' = FADE ANIMATION
+ * 'slide' = SLIDE FROM BOTTOM ANIMATION
+ */
+var option_overlay_animation = 'fade';
+
+/**
+ * OVERLAY CONTENT ANIMATION  [ 'fade' : 'slide' ]
+ * 'fade' = FADE ANIMATION
+ * 'slide' = SLIDE FROM BOTTOM ANIMATION
+ */
+var option_overlay_content_animation = 'slide';
+
+/**
+ * OVERLAY BULLET NAVIGATION [ 'on' : 'off' ]
+ * 'on' = ENABLE BULLET NAVIGATION
+ * 'off' = DISABLE BULLET NAVIGATION
+ */
+var option_overlay_bullet_navigation = 'off';
+
+
+
+/**
+ * HERO CONFIG
+ */
+
+/**
+ * HERO PARALLAX HOVER EFFECT - [ 'on' : 'off' ]
+ * 'on' = OPTION ACTIVATED
+ * 'off' = OPTION DEACTIVATED
+ */
+var option_hero_parallax_hover_effect = 'on';
+
+
+/**
+ * GRAVITY PARTICLE BACKGROUND EFFECT - [ 'on' : 'off' ]
+ * 'on' = OPTION ACTIVATED
+ * 'off' = OPTION DEACTIVATED
+ */
+var option_hero_gravity_effect = 'off';
+
+// Hero background mode
+var option_hero_background_mode = 'glitch';
+
+// Glitch Image
+var option_hero_background_glitch_image = 'assets/images/background.jpg';
+
+
+/** GOOGLE ANALYTICS */
+
+/**
+ * GOOGLE ANALYTICS - [ 'on' : 'off' ]
+ * 'on' = GOOGLE ANALYTICS ON
+ * 'off' = GOOGLE ANALYTICS OFF
+ */
+var option_analytics_tracking = 'off';
+
+/**
+ * TRACKING ID
+ */
+var option_analytics_tracking_id = 'UA-XXXXXXXX-X';
+
+
